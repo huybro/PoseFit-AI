e5ec5572d919b10464a68f27db91c74a8bcb8694
Huy Cao <caogiahuy615@gmail.com>
2025-06-20 14:00:05 +0700

    Initial Commit

---
+++ PoseFit AI/PoseFit_AIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoseFit_AIApp.swift
+//  PoseFit AI
+//
+//  Created by Cao Gia Huy on 6/20/25.
+//
+
+import SwiftUI
+
+@main
+struct PoseFit_AIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PoseFit AITests/PoseFit_AITests.swift
@@ -0,0 +1,17 @@
+//
+//  PoseFit_AITests.swift
+//  PoseFit AITests
+//
+//  Created by Cao Gia Huy on 6/20/25.
+//
+
+import Testing
+@testable import PoseFit_AI
+
+struct PoseFit_AITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
